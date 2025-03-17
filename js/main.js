369be7a093f3015edb1ea6f0420455d42c8dfc27
rct24 <rcristiantraian@yahoo.com>
2025-03-17 16:00:02 +0200

    added canvas

--- js/main.js
+++ js/main.js
@@ -6,10 +6,27 @@ import { setTimer, data } from "./model.js";
 
 const init = function () {
   //setting initial time when opening the app
+  draw();
   setTimer(25, 0);
 
   timerView.addHandlerClick(timerController);
   durationsView.addHandlerClick(durationsController);
 };
+
+function draw() {
+  const canvas = document.getElementById("canvas");
+
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    const radius = 300;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas before drawing
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+}
 
 init();
