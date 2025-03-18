24a7a19622171409f46d205dea8c3077f8fad083
rct24 <rcristiantraian@yahoo.com>
2025-03-18 12:50:31 +0200

    implemented functionality for hours hand

--- js/canvasModel.js
+++ js/canvasModel.js
@@ -1,12 +1,25 @@
 export const data = {
-  _seconds: 0,
-  _minutes: 0,
-  _hours: 0,
-  time: Date.now(),
+  time: {
+    _now: 0,
+    _seconds: 0,
+    _minutes: 0,
+    _hours: 0,
+  },
 
   center: null,
+  radius: 300,
+  canvas: null,
 };
 
 export function getTimeNow() {
-  console.log(new Date(data.time).toLocaleTimeString());
+  const now = new Date();
+  data.time._now = now;
+  data.time._seconds = now.getSeconds();
+  data.time._minutes = now.getMinutes();
+  data.time._hours = now.getHours();
+  console.log(
+    `Local Time: ${data.time._hours}:${data.time._minutes}:${data.time._seconds}`
+  );
 }
+
+//setInterval(getTimeNow, 1000);
--- js/views/canvasView.js
+++ js/views/canvasView.js
@@ -1,32 +1,65 @@
 import * as model from "../canvasModel.js";
 
 class CanvasView {
-  _parentElement = document.getElementById("canvas");
-
   constructor() {
-    model.getTimeNow();
+    //model.getTimeNow();
   }
 
-  draw() {
-    const canvas = document.getElementById("canvas");
-
+  drawWatchFace() {
     if (canvas.getContext) {
       const ctx = canvas.getContext("2d");
       const centerX = canvas.width / 2;
       const centerY = canvas.height / 2;
-      const radius = 300;
+      const radius = model.data.radius;
 
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.beginPath();
       ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
       ctx.lineWidth = 2;
       ctx.strokeStyle = "#0d2d68";
+
+      this.drawHourHand();
       ctx.stroke();
     }
   }
 
+  drawHourHand() {
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+      const centerX = canvas.width / 2;
+      const centerY = canvas.height / 2;
+      const radius = model.data.radius;
+      console.log(
+        "canvas data:",
+        canvas.width,
+        canvas.height,
+        centerX,
+        centerY,
+        radius
+      );
+
+      const hour_rad = this.toRadians(
+        0.5 * (60 * new Date().getHours() + new Date().getMinutes())
+      );
+      const hour_x = centerX + radius * Math.cos(hour_rad);
+      const hour_y = centerY + radius * Math.sin(hour_rad);
+
+      ctx.moveTo(centerX, centerY);
+      ctx.lineTo(hour_x, hour_y);
+      ctx.stroke();
+    }
+  }
+
+  drawSecondsHand() {}
+
+  drawMinuteHand() {}
+
+  toRadians(angle_inDegrees) {
+    return (angle_inDegrees - 90) * (Math.PI / 180);
+  }
+
   addHandlerRender() {
-    window.addEventListener("load", this.draw);
+    window.addEventListener("load", this.drawWatchFace());
   }
 }
 
